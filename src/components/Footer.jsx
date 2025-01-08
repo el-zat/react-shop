8e1bf52782259513da0edd3776229778e92174d2
Elena Zatykina <elena.zatykina@hexagon.com>
2025-01-08 13:26:50 +0100

    add gh-pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,13 @@ function Footer() {
                 <div className="footer-copyright">
                     <div className="container">
                         © {new Date().getFullYear()} Copyright Text
-                        <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+                        <a 
+                            className="grey-text text-lighten-4 right" 
+                            href="https://el-zat.github.io/react-shop/" 
+                            rel="noreferrer" 
+                            target="_blank">
+                            Repo
+                        </a>
                     </div>
                 </div>
   </footer>
